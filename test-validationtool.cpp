121ffb3d575ab66454041c3c69da4ff8c835bfa7
Arun Raveendran <arunr4957@gmail.com>
2021-03-25 23:09:10 +0530

    Update test-validationtool.cpp
    Test case for NULL check.

--- test-validationtool.cpp
+++ test-validationtool.cpp
@@ -14,3 +14,9 @@ TEST_CASE("reports error when current jumps abruptly") {
   int numOfCurReadings = sizeof(currentReadings) / sizeof(currentReadings[0]);
   REQUIRE(validateSOCreadings(currentReadings, numOfCurReadings) == false);
 }
+
+TEST_CASE("reports error when readings are empty") {
+  double *currentReadings = NULL;
+  int numOfCurReadings = 0;
+  REQUIRE(validateSOCreadings(currentReadings, numOfCurReadings) == false);
+}
