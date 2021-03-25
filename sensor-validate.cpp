ee7c6d2b7620b396cc30726ccaffb3d819e7ccca
Arun Raveendran <arunr4957@gmail.com>
2021-03-25 23:00:59 +0530

    Update sensor-validate.cpp
    Broke the functions.

--- sensor-validate.cpp
+++ sensor-validate.cpp
@@ -1,28 +1,37 @@
 #include "sensor-validate.h"
 
-bool _give_me_a_good_name(double value, double nextValue, double maxDelta) {
+bool withInDelta(double value, double nextValue, double maxDelta) {
   if(nextValue - value > maxDelta) {
     return false;
   }
+
   return true;
+}
+
+bool iterCheckForSurge(double* values, int numOfValues, double maxDelta)
+{
+  int lastButOneIndex = numOfValues - 1;
+
+  for(int i = 0; i < lastButOneIndex; i++) {
+    if(!withInDelta(values[i], values[i + 1], 0.05)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+bool validateReadings(double* values, int numOfValues, double maxDelta)
+{
+  if (!values || !numOfValues)
+    return false;
+
+  return iterCheckForSurge(values, numOfValues, maxDelta);
 }
 
 bool validateSOCreadings(double* values, int numOfValues) {
-  int lastButOneIndex = numOfValues - 1;
-  for(int i = 0; i < lastButOneIndex; i++) {
-    if(!_give_me_a_good_name(values[i], values[i + 1], 0.05)) {
-      return false;
-    }
-  }
-  return true;
+  return validateReadings(values, numOfValues, MAX_SOC_DELTA);
 }
 
 bool validateCurrentreadings(double* values, int numOfValues) {
-  int lastButOneIndex = numOfValues - 1;
-  for(int i = 0; i < lastButOneIndex; i++) {
-    if(!_give_me_a_good_name(values[i], values[i + 1], 0.1)) {
-      return false;
-    }
-  }
-  return true;
+  return validateReadings(values, numOfValues, MAX_CURRENT_DELTA);
 }
